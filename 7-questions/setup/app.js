46e8f8f968a4b644a6996192c3f893e50c18e574
and-sang <53617841+and-sang@users.noreply.github.com>
2021-02-10 10:11:27 +0100

    questions project completed

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,32 @@
 //using selectors inside the element
-// traversing the dom
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function() {
+
+        // hide open question before showing a new one
+        questions.forEach(function(q) {
+            if (q !== question) {
+                q.classList.remove('show-text');
+            }
+        });
+
+        question.classList.toggle('show-text');
+    });
+});
+
+
+
+// // traversing the dom
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+
+//     });
+// });
